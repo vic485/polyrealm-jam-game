cc6a82657038ecc44dc720e942e75073c4fbc3c3
Victor Fugazzotto <victorfuga@gmail.com>
2019-03-02 08:20:08 -0500

    Basic character

---
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Controller
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private CharacterController _controller;
+        public float lookSpeed = 9f;
+
+        private void Start()
+        {
+            _controller = GetComponent<CharacterController>();
+        }
+
+        private void Update()
+        {
+            _controller.Move(new Vector3(0f, 0f, Input.GetAxis("Vertical")));
+            var yRot = transform.rotation.eulerAngles.y;
+            yRot += Input.GetAxis("Horizontal") * lookSpeed;
+            transform.rotation = Quaternion.Euler(0f, yRot, 0f);
+        }
+    }
+}
