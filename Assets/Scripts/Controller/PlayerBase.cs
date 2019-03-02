7df4bde2c5346b3c60193244e2c4c3b3db4ee39d
Victor Fugazzotto <victorfuga@gmail.com>
2019-03-02 16:44:50 -0500

    Xbox controller input

---
+++ Assets/Scripts/Controller/PlayerBase.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Controller
+{
+    public class PlayerBase : MonoBehaviour
+    {
+        public float Health { get; private set; }
+
+        public void Attack(float damage)
+        {
+            Health -= damage;
+        }
+    }
+}
