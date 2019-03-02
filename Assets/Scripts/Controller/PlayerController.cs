de099b293c1a0dc573f46bcb9530ebfc1e646a53
Luuk van Oijen <lazyluuk.channel@gmail.com>
2019-03-02 15:11:53 +0100

    epic
    fixed ur controller vic, no problem bb

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -5,19 +5,29 @@ namespace Controller
     public class PlayerController : MonoBehaviour
     {
         private CharacterController _controller;
-        public float lookSpeed = 9f;
+        private Transform _camera;
+        public GameObject body;
+        public GameObject eye;
+        public float lookSpeed = 5f;
+        public float moveSpeed = 1f;
 
         private void Start()
         {
-            _controller = GetComponent<CharacterController>();
+            _controller = body.GetComponent<CharacterController>();
+            _camera = eye.transform;
         }
 
         private void Update()
         {
-            _controller.Move(new Vector3(0f, 0f, Input.GetAxis("Vertical")));
-            var yRot = transform.rotation.eulerAngles.y;
-            yRot += Input.GetAxis("Horizontal") * lookSpeed;
-            transform.rotation = Quaternion.Euler(0f, yRot, 0f);
+            _camera.rotation = Quaternion.Lerp(_camera.rotation, body.transform.rotation, Time.deltaTime * 20f); //Lerp so that the jerkiness of 30 updates per second doesn't make the game feel like 30FPS on a 60+hz monitor
+        }
+
+        private void FixedUpdate()
+        {
+            _controller.Move(transform.forward * Input.GetAxis("Vertical") * moveSpeed * Time.fixedDeltaTime);
+            var yRot = body.transform.rotation.eulerAngles.y;
+            yRot += Input.GetAxis("Horizontal") * lookSpeed * Time.fixedDeltaTime;
+            body.transform.rotation = Quaternion.Euler(0f, yRot, 0f);
         }
     }
 }
