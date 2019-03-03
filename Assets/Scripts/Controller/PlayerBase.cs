fb04133473e872c652328b6d36ff43f62517b45e
Victor Fugazzotto <victorfuga@gmail.com>
2019-03-02 19:14:45 -0500

    Player stats start

--- Assets/Scripts/Controller/PlayerBase.cs
+++ Assets/Scripts/Controller/PlayerBase.cs
@@ -4,11 +4,17 @@ namespace Controller
 {
     public class PlayerBase : MonoBehaviour
     {
-        public float Health { get; private set; }
+        public float Health { get; protected set; } = 100f;
+        public float CloseAttack { get; protected set; } = 10f;
+        public float RangeAttack { get; protected set; } = 50f;
+        public float MagicAttack { get; protected set; } = 25f;
+        public float Armor { get; private set; } = 0f;
 
         public void Attack(float damage)
         {
-            Health -= damage;
+            Health -= Mathf.Min(damage - Armor, 0f);
+            
+            // TODO: Die when health falls below 0
         }
     }
 }
---
+++ Assets/Scripts/Controller/Warrior.cs
@@ -0,0 +1,14 @@
+namespace Controller
+{
+    /// <summary>
+    /// Warrior has 
+    /// </summary>
+    public class Warrior : PlayerBase
+    {
+        // Make changes to player's stats
+        private void Start()
+        {
+            CloseAttack = 50f;
+        }
+    }
+}
