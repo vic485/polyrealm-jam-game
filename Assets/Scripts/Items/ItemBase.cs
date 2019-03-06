6641593af8482c19c646c3790c189ef39bb733d5
Luuk van Oijen <lazyluuk.channel@gmail.com>
2019-03-06 16:29:10 +0100

    ITEM BASE
    EPIC

--- Assets/Scripts/Items/ItemBase.cs
+++ Assets/Scripts/Items/ItemBase.cs
@@ -6,9 +6,30 @@ namespace Items
 {
     public class ItemBase : MonoBehaviour
     {
+        private Transform _playerTransform;
+
+        private void Start()
+        {
+            _playerTransform = GameObject.FindWithTag("Player").transform;
+        }
+
         private void OnTriggerEnter(Collider other)
         {
-            
+            if (other.gameObject.tag == "Player")
+            {
+                //Item is being picked up
+                OnPickup();
+            }
+        }
+
+        protected void OnPickup()
+        {
+            Destroy(gameObject);
+        }
+
+        protected void Update()
+        {
+            transform.forward = _playerTransform.forward;
         }
     }
 }
